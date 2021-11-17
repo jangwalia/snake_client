3fd3f824345fa6e2d69be0f55f3bbc33c0d07e8d
jang walia <jangiwalia83@gmail.com>
2021-11-17 07:30:01 -0800

    getting msg from server

--- play.js
+++ play.js
@@ -3,11 +3,11 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
+    host: 'localhost',
     port: 50541
   });
   //get a msg from server when connected
-  conn.on('connect',(data)=>{
+  conn.on('data',(data)=>{
     console.log(data);
   })
   // interpret incoming data as text
