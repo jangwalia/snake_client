e25036fcc9968f780038dff61644d3e06dcd919a
jang walia <jangiwalia83@gmail.com>
2021-11-18 08:51:28 -0800

    Commiting WASD movements

--- play.js
+++ play.js
@@ -1,5 +1,5 @@
-const {myfunc} = require('./client');
-const{setupInput} = require('./input');
+const { myfunc } = require('./client');
+const { setupInput } = require('./input');
 // establishes a connection with the game server
 //calling the function using destructuring 
 console.log('connecting....')
